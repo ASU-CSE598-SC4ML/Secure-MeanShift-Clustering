92f4ffc48da5b06c66e50662e889df5a68887328
zlijingtao <lijingtaoz@hotmail.com>
2021-04-06 22:54:36 -0700

    create share for two parties, done in a new machine, with a new environment, some usage are different

--- mod2_distcal.py
+++ mod2_distcal.py
@@ -13,8 +13,8 @@ from mod1_generate import point_gen
 import crypten
 import crypten.mpc as mpc
 import crypten.communicator as comm 
-import crypten.mpc.primitives.ot.baseOT as baseOT
-from crypten.common.rng import generate_kbit_random_tensor, generate_random_ring_element
+# import crypten.mpc.primitives.ot.baseOT as baseOT
+# from crypten.common.rng import generate_kbit_random_tensor, generate_random_ring_element
 
 
 def sample_dust(point_array, n_dusts):
@@ -23,7 +23,8 @@ def sample_dust(point_array, n_dusts):
 
 class distance_calculation(object):
     def __init__(self, n_point = 2, n_dust = 1, n_center = 1):
-        
+        crypten.init()
+        torch.set_num_threads(1)
         if_plot = True
         self.upper_x = 1
         self.lower_x = 0
@@ -50,49 +51,16 @@ class distance_calculation(object):
             dust = list(self.dust_array[i, :])
             for j in range(self.n_point):
                 point =  list(self.point_array[j, :])
-                point_enc = crypten.cryptensor(point, ptype=crypten.mpc.arithmetic)
-                dust_enc = crypten.cryptensor(dust, ptype=crypten.mpc.arithmetic)
+                point_enc = crypten.cryptensor(point, ptype=crypten.ptype.arithmetic)
+                dust_enc = crypten.cryptensor(dust, ptype=crypten.ptype.arithmetic)
                 dist_share_list.append(point_enc)
                 # x_enc = crypten.cryptensor([2, 3], ptype=crypten.mpc.binary)
         res_share_list.append(dist_share_list)
         rank = comm.get().get_rank()
-        crypten.print(f"\nRank {rank}:\n {str(res_share_list)}\n")
-                # dist_share0, dist_share1 = [0.1, 0.09]
-                # dist_share0_list.append(dist_share0)
-                # dist_share1_list.append(dist_share1)
-            # res_share_list.append(dist_share_list)
-        # return np.array(res_share0_list), np.array(res_share1_list)
-    # pass
-
-@mpc.run_multiprocess(world_size=2)
-def examine_arithmetic_shares():
-    x_enc = crypten.cryptensor([1, 2, 3], ptype=crypten.mpc.arithmetic)
-    
-    rank = comm.get().get_rank()
-    crypten.print(f"\nRank {rank}:\n {x_enc}\n")
+        print(f"\nRank {rank}:\n {str(res_share_list)}\n")
 
 if __name__ == '__main__':
-
-    #initialize crypten
-    # OMP_NUM_THREADS=2
-    #Disables OpenMP threads -- needed by @mpc.run_multiprocess which uses fork
-    crypten.init()
-    torch.set_num_threads(1)
     dist_cal1 = distance_calculation()
-    # arr1, arr2 = dist_cal1.fit()
     dist_cal1.fit()
-    # print(arr1.shape, arr2.shape)
-    # print(torch.get_num_threads())
-    
-    x = examine_arithmetic_shares()
-
-    # ot = baseOT.BaseOT((self.rank + 1) % self.world_size)
-    # msg0s = ["9"]
-    # msg1s = ["23"]
-    # ot.send(msg0s, msg1s)
-    # choices = [1]
-    
-    # msgcs = ot.receive(choices)
-    # print(msgcs)
 # %%
 
