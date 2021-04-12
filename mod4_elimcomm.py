1514f47fa757ccbc1ae09add4ac1802459a5bd53
zlijingtao <lijingtaoz@hotmail.com>
2021-04-12 15:34:36 -0700

    finished elimcomm

--- mod4_elimcomm.py
+++ mod4_elimcomm.py
@@ -1,9 +1,5 @@
 #%%
 
-#Example Usage
-# _ , gt_centroid = point_gen([0,1], [0,1], 4, 1000, if_plot = True)
-# print("Ground-truth centroid is: ", gt_centroid)
-
 import numpy as np
 import numpy as np
 import torch
@@ -16,41 +12,61 @@ import pickle
 
 
 class eliminate_common(object):
-    def __init__(self,n_point = 2, n_dust = 1,radius=0.1):
+    def __init__(self,radius=0.1):
         crypten.init()
-        # with open('dist_rank_0.pickle', 'rb') as handle:
-        #     temp_dict = pickle.load(handle)
-        # temp_dist_enc = temp_dict["distance_share_list_rank0"]
-        # self.n_point = len(temp_dist_enc[0])
-        # self.n_dust = len(temp_dist_enc)
-        # self.radius = radius
         torch.set_num_threads(1)
+        self.radius = radius
+        self.n_dust = 10 # match n_dust in fake_gen, will set if last step's share is provided
+    #Fake Data
+    @mpc.run_multiprocess(world_size=2)  # Two process will run the identical code below:
+    def fake_gen(self, verify = False):
+        rank = comm.get().get_rank()
+        centroid_share_list = []
+        for i in range(10):
+            dust_enc = crypten.cryptensor((i%5)/10, ptype=crypten.ptype.arithmetic)
+            centroid_share_list.append(dust_enc)
+        return_dict = {}
+        return_dict["fake_centroid_share_list_rank{}".format(rank)] = centroid_share_list
+        with open('fake_centroid_rank_{}.pickle'.format(rank), 'wb') as handle:
+            pickle.dump(return_dict, handle, protocol=pickle.HIGHEST_PROTOCOL)
+        
 
     @mpc.run_multiprocess(world_size=2)  # Two process will run the identical code below:
     def eliminate(self, verify = False):
         rank = comm.get().get_rank()
 
         #Receive secret share from compare step.
+        with open('fake_centroid_rank_{}.pickle'.format(rank), 'rb') as handle:
+            ss_dict = pickle.load(handle)
+        old_centroid_share_list = ss_dict["fake_centroid_share_list_rank{}".format(rank)]
+        
+        
+        #Eliminate Repeated Centroid
+        centroid_share_list = []
+        for i in range(self.n_dust - 1):
+            temp_ss = crypten.cryptensor(0.0, ptype=crypten.ptype.arithmetic)
+            for j in range(i+1, self.n_dust):
+                temp_ss += (old_centroid_share_list[i] == old_centroid_share_list[j])
+            if int(temp_ss.get_plain_text().item()) == 0:
+                centroid_share_list.append(old_centroid_share_list[i])
+        centroid_share_list.append(old_centroid_share_list[-1])
 
-        # with open('data_rank_{}.pickle'.format(rank), 'rb') as handle:
-        #     ss_dict = pickle.load(handle)
-        # dust_share_list = ss_dict["dust_share_list_rank{}".format(rank)]
-        # point_share_list = ss_dict["point_share_list_rank{}".format(rank)]
-
-        # Verify distance calculation the secret share.
         if verify:
             if rank == 0:
                 print("=========Start of Verification========")
-            true_distance = sum(((point_share_list[0].get_plain_text() - point_share_list[1].get_plain_text())**2))
-            dist_sum = (point_share_list[0] ** 2 +  point_share_list[1] ** 2 - 2 * (point_share_list[0] * point_share_list[1])).sum()
-            dist_sum_val = dist_sum.get_plain_text()
+                print("--------Input--------")
+            for i in range(self.n_dust):
+                centroid_val = old_centroid_share_list[i].get_plain_text()
+                if rank == 0:
+                    print("Centroid True Value is: ", centroid_val)
             if rank == 0:
-                print("Ground-Truth Distance is: ", true_distance)
-                print("Decrypted Distance is: ", dist_sum_val)
+                print("--------Output--------")
+            for i, share in enumerate(centroid_share_list):
+                result_val = share.get_plain_text()
+                if rank == 0:
+                    print("Resulted Value is: ", result_val)
+            if rank == 0:
                 print("=========End of Verification========")
-
-        #Eliminate Repeated Centroid
-        pass
 
         #Save New Centroid
         return_dict = {}
@@ -59,10 +75,9 @@ class eliminate_common(object):
             pickle.dump(return_dict, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
 if __name__ == '__main__':
-    pass
     #Example Usage
-    # dist_cal1 = distance_calculation()
-    # dist_cal1.enc()
-    # dist_cal1.discal()
+    eliminate_common1 = eliminate_common()
+    eliminate_common1.fake_gen()
+    eliminate_common1.eliminate(True)
 # %%
 
