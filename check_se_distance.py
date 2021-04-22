2fd10cf38ec034a20ed0d891a2c74dd65be11568
q <fuqiqiqiang@gmail.com>
2021-04-22 01:57:55 -0700

    fixed stuff, prints the centroid when updated

--- check_se_distance.py
+++ check_se_distance.py
@@ -28,14 +28,24 @@ class compare_radius(object):
         with open('dist_rank_{}.pickle'.format(rank), 'rb') as handle:
             dist_dict = pickle.load(handle)
         dist_enc = dist_dict["distance_share_list_rank{}".format(rank)]
+        dist_enc_new = crypten.cryptensor(torch.ones(self.n_dust, self.n_point))
+        for i in range(self.n_dust):
+            for j in range(self.n_point):
+                dist_enc_new[i,j]=dist_enc[i][j]
 
         with open('data_rank_{}.pickle'.format(rank), 'rb') as handle:
             ss_dict = pickle.load(handle)
         point_enc = ss_dict["point_share_list_rank{}".format(rank)]
+        point_enc_new = crypten.cryptensor(torch.ones(self.n_point, 2))
+        for i in range(self.n_point):
+            point_enc_new[i,:]=point_enc[i][:]
 
         with open('centroid_rank_{}.pickle'.format(rank), 'rb') as handle:
             ss_dict = pickle.load(handle)
         dust_enc = ss_dict["centroid_share_list_rank{}".format(rank)]
+        dust_enc_new = crypten.cryptensor(torch.ones(self.n_dust, 2))
+        for i in range(self.n_dust):
+            dust_enc_new[i,:]=dust_enc[i][:]
 
         #temp is the radius
         distance_bool_list = []
@@ -48,32 +58,44 @@ class compare_radius(object):
             radius_enc = crypten.cryptensor(temprad, ptype=crypten.ptype.arithmetic)
             
             #calculates if point distance is le radius
-            temp_bool = dist_enc[i]<=radius_enc
+            temp_bool = crypten.cryptensor(temprad, ptype=crypten.ptype.arithmetic)
+            temp_bool = dist_enc_new[i]<=radius_enc
             
             #multiply point value with 0/1 matrix, 
             #result should be the updated centroid location
-            temp_pts = crypten.cryptensor(torch.ones(point_enc[i].shape))
-            for i in range(point_enc.shape[0]):
-                temp_pts[i] = enc_pts[i,:]*temp_bool[i]
+            temp_pts = crypten.cryptensor(torch.ones(point_enc_new.shape))
+            for j in range(self.n_point):
+                temp_pts[j,:] = point_enc_new[j,:]*temp_bool[j]
             
             #sum them up, divide by sum of 0/1 matrix
             updated_centroid = (temp_pts.sum(0))/temp_bool.sum()
+            """if debug:
+                a = updated_centroid.get_plain_text()
+                b = dust_enc_new[i,:].get_plain_text()
+                if rank==0:
+                    print("a and b:")
+                    print(a)
+                    print(b)"""
             #get plain text of ne, if is 1(not equal) then set changed
-            if (updated_centroid!=dust_enc[i]).get_plain_text().item():
+            changetemp = (updated_centroid!=dust_enc_new[i,:]).get_plain_text().sum().item()
+            if (changetemp):
                 #if changed, set flag and change dust
                 changed = True
-                dust_enc[i] = updated_centroid
                 if debug:
-                    print(updated_centroid)
-                    print(dust_enc[i])
-            updated_dust_list.append(templist)
+                    a = updated_centroid.get_plain_text()
+                    b = dust_enc_new[i,:].get_plain_text()
+                    if rank==0:
+                        print("a and b:")
+                        print(a)
+                        print(b)
+                dust_enc[i] = updated_centroid
         
         #if changed, then update the data file with new dust
         if changed:
             ss_dict["centroid_share_list_rank{}".format(rank)]= dust_enc
             with open('centroid_rank_{}.pickle'.format(rank), 'wb') as handle:
                 pickle.dump(ss_dict, handle, protocol=pickle.HIGHEST_PROTOCOL)
-        with open('result.pickle', 'wb') as handle:
+            with open('result.pickle', 'wb') as handle:
                 pickle.dump(changed, handle, protocol=pickle.HIGHEST_PROTOCOL)
     
     #The verify is still incorrect, I'll fix it and test if the code works correctly
